1236cd1889abed0e90d1a6dee46da06a6588fad4
doveking9999 <hunwoo45@naver.com>
2025-09-09 01:22:59 +0900

    토글 애니 스위치 적용
    ㅇㅇ

--- Assets/Scripts/UI/TogglePhone.cs
+++ Assets/Scripts/UI/TogglePhone.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class TogglePhone : MonoBehaviour
 {
+    [SerializeField] private Animator anim; // 폰 오브젝트 할당
     [SerializeField] private GameObject phone; // 폰 오브젝트 할당
     private bool isActive = false;
 
@@ -10,7 +11,16 @@ public class TogglePhone : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Tab))
         {
             isActive = !isActive;
-            phone.SetActive(isActive);
+
+            if (isActive)
+            {
+                anim.Play("Open");
+            }
+            else 
+            {
+                anim.Play("Close");
+            }
+            
         }
     }
 }
