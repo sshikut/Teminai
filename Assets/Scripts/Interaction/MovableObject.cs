bb67e91b6437428da3b96c1f64090d55d81bcc99
sshikut <sungsuhyun12@gmail.com>
2025-09-23 11:21:49 +0900

    Update Screen Interaction

---
+++ Assets/Scripts/Interaction/MovableObject.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+using System.Collections;
+
+public class MovableObject : MonoBehaviour, IInteractable
+{
+    public GameObject screen;
+
+    public float duration = 5f;
+
+    public Vector3 originalPosition;
+    public Vector3 targetPosition;
+
+    private Coroutine moveCoroutine;
+    public bool hasMoved = false;
+
+    public void Interact() // 인터페이스 함수
+    {
+        if (!hasMoved)
+        {
+            StartMovement();
+            hasMoved = true;
+        }
+        else
+        {
+            ResetPosition();
+            hasMoved = false;
+        }
+    }
+
+    public void StartMovement()
+    {
+        if (moveCoroutine != null)
+        {
+            StopCoroutine(moveCoroutine);
+        }
+        moveCoroutine = StartCoroutine(MoveTo(targetPosition));
+    }
+
+    
+    public void ResetPosition()
+    {
+        if (moveCoroutine != null)
+        {
+            StopCoroutine(moveCoroutine);
+        }
+
+        moveCoroutine = StartCoroutine(MoveTo(originalPosition));
+    }
+
+    
+    private IEnumerator MoveTo(Vector3 target)
+    {
+
+        float elapsedTime = 0f; 
+
+        while (elapsedTime < duration)
+        {
+            screen.transform.position = Vector3.Lerp(screen.transform.position, target, elapsedTime / duration);
+
+            elapsedTime += Time.deltaTime;
+
+            yield return null;
+        }
+
+        screen.transform.position = target;
+    }
+}
