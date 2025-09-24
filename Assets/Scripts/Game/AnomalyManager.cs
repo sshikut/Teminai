ccef0ba7436eb8c8314baec7e3139d30d55767d4
sshikut <sungsuhyun12@gmail.com>
2025-09-25 01:59:58 +0900

    Update RDSS

--- Assets/Scripts/Game/AnomalyManager.cs
+++ Assets/Scripts/Game/AnomalyManager.cs
@@ -7,6 +7,7 @@ public class AnomalyManager : MonoBehaviour
 {
     [Header("Game Settings")]
     public AnomalyMethod anomaly;
+    public RDSS rdss;
     public int loopCount = 0;
     public int absentCount = 0;
     public int clearCount = 8;
@@ -48,9 +49,10 @@ public class AnomalyManager : MonoBehaviour
 
     public void Anomaly()
     {
+        rdss.RandomSituation();
         anomaly.InitAnomaly(); // 이상현상 초기화
         timerManager.timerText.text = null;
-        Debug.Log(triggerZone.triggerOnce + "이상현상매니저");
+
         if (loopCount >= clearCount)
         {
             Clear();
--- Assets/Scripts/System/RDSS.cs
+++ Assets/Scripts/System/RDSS.cs
@@ -1,18 +1,27 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class RDSS : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject[] situations;
+    public int random;
+
+    // RDS_0 = 교수는 강의 중
+    // RDS_1 = 교수는 쉬는 중
+    // RDS_2 = 교수는 강의하러 오는 중
+    // RDS_3 = 임시
+    // RDS_4 = 임시
+
+    public void RandomSituation()
     {
-        
+        InitSituation();
+
+        random = Random.Range(0, situations.Length);
+
+        situations[random].SetActive(true);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void InitSituation()
     {
-        
+        situations[random].SetActive(false);
     }
 }
