1a12845b8211cfef062e0de15602ad04ce74f269
doveking9999 <hunwoo45@naver.com>
2025-10-04 11:11:10 +0900

    콜라이더 범위 수정 및 와이어 스크립트 수정
    ㅇㅇ

--- Assets/Scripts/Waruma/Trigger.cs
+++ Assets/Scripts/Waruma/Trigger.cs
@@ -2,16 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-using UnityEngine;
-
 public class Trigger : MonoBehaviour
 {
+    private Vector3 lastPlayerPos;    // 마지막 위치 저장
+    private bool playerInside = false;
+
     private void OnTriggerEnter(Collider other)
     {
-        // 들어온 오브젝트가 Player 태그인지 확인
         if (other.CompareTag("Player"))
         {
             Debug.Log("플레이어가 트리거 존에 들어옴!");
+            playerInside = true;
+            lastPlayerPos = other.transform.position;
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (playerInside && other.CompareTag("Player"))
+        {
+            Vector3 currentPos = other.transform.position;
+
+            // 조금이라도 움직였는지 체크
+            if (Vector3.Distance(currentPos, lastPlayerPos) > 0.01f)  // 0.01은 민감도
+            {
+                Debug.Log("플레이어가 트리거 존에서 움직였습니다!");
+                lastPlayerPos = currentPos;
+            }
         }
     }
 
@@ -20,6 +37,7 @@ public class Trigger : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             Debug.Log("플레이어가 트리거 존에서 나감!");
+            playerInside = false;
         }
     }
 }
