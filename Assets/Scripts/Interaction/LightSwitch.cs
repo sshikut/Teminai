6c0827266b09dae8a43993000e3e2ab2c8974e1e
doveking9999 <hunwoo45@naver.com>
2025-10-17 15:21:53 +0900

    스위치 좌우 반전 추가
    ㅇㅇ

--- Assets/Scripts/Interaction/LightSwitch.cs
+++ Assets/Scripts/Interaction/LightSwitch.cs
@@ -11,6 +11,12 @@ public class LightSwitch : MonoBehaviour, IInteractable
     public void Interact()
     {
         isLightOn = !isLightOn;
+
+        // 상호작용할 때마다 좌우 반전
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
+
         AudioManager.instance.Play("LightSwitch");
         float targetIntensity = isLightOn ? 2f : 0f;
 
