d4611ff4087495f1f0ce929eb346b26c33b6b866
sshikut <sungsuhyun12@gmail.com>
2025-10-17 19:32:43 +0900

    Update Poop

--- Assets/Scripts/Interaction/PoopSurvival.cs
+++ Assets/Scripts/Interaction/PoopSurvival.cs
@@ -7,10 +7,14 @@ public class PoopSurvival : MonoBehaviour, IInteractable
     // 급똥 이상 현상일 때 해당 오브젝트가 활성화되며 상호 작용이 가능함
     public TimerManager timer;
     public GameObject trigger;
+    public GameObject botherTrigger;
 
     public void Interact()
     {
         timer.StartTimer();
         trigger.SetActive(true);
+        botherTrigger.SetActive(true);
+
+        this.enabled = false;
     }
 }
