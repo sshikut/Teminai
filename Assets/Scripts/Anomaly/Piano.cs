4a581524c0c0c6a2fa0fa01506c3fedcffc00a1d
doveking9999 <hunwoo45@naver.com>
2025-10-19 11:48:09 +0900

    피아노 푸쉬

--- Assets/Scripts/Anomaly/Piano.cs
+++ Assets/Scripts/Anomaly/Piano.cs
@@ -1,43 +1,52 @@
-using System.Collections;
-using System.Collections.Generic;
-using StarterAssets;
 using UnityEngine;
 
-public class Piano : MonoBehaviour, IInteractable
+public class PianoKey : MonoBehaviour
 {
-    public bool isOn = false;
+    public string soundName = "Do";   // 누를 때 재생할 효과음 이름
+    public float pressDepth = 0.02f;  // 얼마나 눌릴지
+    public float pressSpeed = 10f;    // 눌림 속도
 
-    public void Interact()
+    private Vector3 originalPosition;
+    private bool isPressed = false;
+
+    void Start()
     {
-        isOn = !isOn;
+        originalPosition = transform.localPosition;
     }
 
-
-    // Update is called once per frame
-    void Update()
+    void OnMouseDown()
     {
-        if (isOn)
+        if (!isPressed)
         {
-            if (Input.GetKeyDown(KeyCode.Alpha1))
-                AudioManager.instance.Play("Do");
+            // 사운드 재생
+            AudioManager.instance.Play(soundName);
 
-            if (Input.GetKeyDown(KeyCode.Alpha2))
-                AudioManager.instance.Play("Re");
-
-            if (Input.GetKeyDown(KeyCode.Alpha3))
-                AudioManager.instance.Play("Mi");
-
-            if (Input.GetKeyDown(KeyCode.Alpha4))
-                AudioManager.instance.Play("Fa");
-
-            if (Input.GetKeyDown(KeyCode.Alpha5))
-                AudioManager.instance.Play("Sol");
-
-            if (Input.GetKeyDown(KeyCode.Alpha6))
-                AudioManager.instance.Play("Ra");
-
-            if (Input.GetKeyDown(KeyCode.Alpha7))
-                AudioManager.instance.Play("Si");
+            // 눌림 애니메이션 실행
+            StartCoroutine(PressKey());
         }
+    }
+
+    private System.Collections.IEnumerator PressKey()
+    {
+        isPressed = true;
+        Vector3 target = originalPosition - new Vector3(0, pressDepth, 0);
+
+        // 아래로 눌림
+        while (Vector3.Distance(transform.localPosition, target) > 0.001f)
+        {
+            transform.localPosition = Vector3.Lerp(transform.localPosition, target, Time.deltaTime * pressSpeed);
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(0.05f);
+
+        // 다시 위로
+        while (Vector3.Distance(transform.localPosition, originalPosition) > 0.001f)
+        {
+            transform.localPosition = Vector3.Lerp(transform.localPosition, originalPosition, Time.deltaTime * pressSpeed);
+            yield return null;
+        }
+
+        isPressed = false;
     }
 }
