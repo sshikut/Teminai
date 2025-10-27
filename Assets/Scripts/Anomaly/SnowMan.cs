e6db3dea5ff9382ec50950ce05c8314cb7ec93fc
doveking9999 <hunwoo45@naver.com>
2025-10-27 09:06:31 +0900

    눈사람 땅콩이처럼 구현

--- Assets/Scripts/Anomaly/SnowMan.cs
+++ Assets/Scripts/Anomaly/SnowMan.cs
@@ -3,8 +3,10 @@ using UnityEngine.AI;
 
 public class SnowMan : MonoBehaviour
 {
-    public Transform target;
+    public Transform player;
     private NavMeshAgent agent;
+
+    public float viewAngle = 45f; 
 
     void Start()
     {
@@ -13,7 +15,21 @@ public class SnowMan : MonoBehaviour
 
     void Update()
     {
-        if (target != null)
-            agent.SetDestination(target.position);
+        if (!player) return;
+
+        // 플레이어가 바라보는 방향과 눈사람 방향의 각도 계산
+        Vector3 toSnowman = (transform.position - player.position).normalized;
+        float angle = Vector3.Angle(player.forward, toSnowman);
+
+        // 각도가 작으면 보고 있는 거니까 멈추고, 아니면 추격
+        if (angle < viewAngle)
+        {
+            agent.isStopped = true; // 멈춤
+        }
+        else
+        {
+            agent.isStopped = false; // 이동
+            agent.SetDestination(player.position);
+        }
     }
 }
